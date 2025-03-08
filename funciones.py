edaae7b1a74e9ccc9ec0cc5967fcfbdc7c86b9a6
santiagotrading <santiagohernandeztobon24@gmail.com>
2025-03-08 09:41:39 -0500

    us-001/create_register firts commit

---
+++ funciones.py
@@ -0,0 +1,20 @@
+
+
+
+#us-001/creat_register
+
+user = []
+
+def user_regirter():
+  id = int(input("ingresar id"))
+  user.append(id)
+  name = input("agregar su nombre")
+  user.append(name)
+  last_name = input("agrega tu apellido")
+  user.append(last_name)
+  email = input("agrega su email")
+  user.append(email)
+  password = input("cree un password de 8 caracteres")
+  user.append(password)
+  print("usuario creado con exito")
+  
