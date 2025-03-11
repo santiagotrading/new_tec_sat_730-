7ffe94f40ba44a7a8b2ae719c24c28fa367be10f
santiagotrading <santiagohernandeztobon24@gmail.com>
2025-03-10 20:04:34 -0500

    historia de usuario 2

--- funciones.py
+++ funciones.py
@@ -17,4 +17,8 @@ def user_regirter():
   password = input("cree un password de 8 caracteres")
   user.append(password)
   print("usuario creado con exito")
-  
+  print(user)
+
+#creando login
+def user_login():
+  print("login")  
